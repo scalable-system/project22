6d601a7b1939d074f0b3c4dabf0041d55d428c80
Din Spataj <proendergamer03@gmail.com>
2020-02-20 11:42:41 +0300

    Update
    - Added Bootstrap  CDN (Note: CDN is quicker than downloading the files)
    
    -I have created a onInput function for displaying what is being typed

--- index.php
+++ index.php
@@ -4,17 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Excel Demo</title>
+
+    <!-- Css and Bootstrap -->
     <link rel="stylesheet" href="styles/style.css">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
         <p>Whatever you enter here will be inserted in the excel</p>
-        <input type="text" name="value" id="" placeholder="enter something">
-        <input type="text" name="cell" id="" placeholder="enter the cell">
-        <input type="submit" value="submit">
+        <input class="form-control" type="text" name="value" id="value" placeholder="enter something" oninput="onInput(this.id, this.id+'Output');">
+        <input class="form-control" type="text" name="cell" id="value1" placeholder="enter the cell" oninput="onInput(this.id, this.id+'Output');">
+        <input type="submit" class="btn btn-primary btn-lg btn-block" value="submit">
     </form>
     
+    <h3 id="valueOutput"></h3>
+    <h3 id="value1Output"></h3>
+
+    <!-- Bootsrap and JQuery for bootstrap -->
+    <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="js/inserting.js" type="text/javascript"></script>
 </body>
 </html>
 
---
+++ js/inserting.js
@@ -0,0 +1,4 @@
+function onInput(input, output) {
+    var x = document.getElementById(input).value;
+    document.getElementById(output).innerHTML = x;
+}
