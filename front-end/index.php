591a1a12e3a1a13d3f91d31fd9a2e9b34c372663
TAHIDUR <tahidurrahman.tuhin@gmail.com>
2020-02-24 02:37:09 +0600

    Update index.php

--- front-end/index.php
+++ front-end/index.php
@@ -11,24 +11,26 @@
     <link rel="stylesheet" href="css/style.css" />
   </head>
 
-  <body>
+  <body onload='document.form.product-title-1.focus()'>
     <script src="js/scripts.js"></script>
     <div class="container-fluid"> 
       <div class="row">
         <div class="col-sm-6"> 
-          <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+          <form name="form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
             <p>Whatever you enter here will be inserted in the excel</p>
-            <input class="form-control" type="text" name="product-title-1" id="product-title-1" placeholder="Product Title" onclick="stringlength(document.form1.text1,10,200)" onkeyup="document.getElementById('productOutput').innerHTML = this.value">
-            <input class="form-control" type="text" name="quantity-1" id="quantity-1" placeholder="Quantity" onclick="stringlength(document.form1.text1,1,20)" onkeyup="document.getElementById('quantityOutput').innerHTML = this.value">
-            <input type="submit" class="btn btn-primary btn-lg btn-block" value="submit">
+            <input class="form-control" type="text" name="product-title-1" id="product-title-1"  placeholder="Product Title"  onkeyup="document.getElementById('productOutput').innerHTML = this.value" >
+            <input class="form-control" type="number" name="quantity-1" id="quantity-1" placeholder="Quantity"  onkeyup="document.getElementById('quantityOutput').innerHTML = this.value ">
+            <input onclick="filter()"  type="submit" class="btn btn-primary btn-lg btn-block" value="submit">
         </form>
         </div>
         <div class="col-sm-6">
           <h3 id="productOutput"></h3>
           <h3 id="quantityOutput"></h3>
         </div>
+        <h3 id="Alert"></h3>
       </div>
     </div>
+
     <!-- Bootsrap and JQuery for bootstrap -->
     <script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
