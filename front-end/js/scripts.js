0bd44bc93afb07d4baab677207c7417edae45155
Din Spataj <proendergamer03@gmail.com>
2020-02-27 16:06:39 +0300

    Validation

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -1,15 +1,110 @@
-function filter() {
-    
-    var product = document.getElementById('product-title-1').value;
-    var quantity = document.getElementById('quantity-1').value;
+function validation(index, id, divId, type, min, max, name){
+    var myInput = document.getElementById(id);
+    var letter = document.getElementById(divId);
 
-    proSize = product.length;
-    quantitySize = quantity.length;
+    // When the user starts to type something inside the password field
+    myInput.oninput = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {  
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-danger");
+        letter.classList.add("alert-success");
+        letter.classList.add("valid");
+        letter.classList.remove("invalid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-success");
+        letter.classList.add("alert-danger");
+        letter.classList.add("invalid");
+    }
     
-    if(proSize < 10 || proSize > 200 || quantitySize <0 || quantitySize > 20 )
-        alert("bad");
-    else
-        alert("good");
-        var product_title_1_js = product;
-        var product_title_1_js = quantity;
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {  
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-danger");
+        letter.classList.add("alert-success");
+        letter.classList.add("valid");
+        letter.classList.remove("invalid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-success");
+        letter.classList.add("alert-danger");
+        letter.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {  
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-danger");
+        letter.classList.add("alert-success");
+        letter.classList.add("valid");
+        letter.classList.remove("invalid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.remove("alert-warning");
+        letter.classList.remove("alert-success");
+        letter.classList.add("alert-danger");
+        letter.classList.add("invalid");
+    }
+    
+    // Validate length
+    if(type == 1){
+        if(myInput.value.length >= min && myInput.value.length <= max) {
+            letter.classList.remove("alert-warning");
+            letter.classList.remove("alert-danger");
+            letter.classList.add("alert-success");
+            letter.classList.add("valid");
+            letter.classList.remove("invalid");
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.remove("alert-warning");
+            letter.classList.remove("alert-success");
+            letter.classList.add("alert-danger");
+            letter.classList.add("invalid");
+        }
+    }else if(type == 2){
+        if(myInput.value >= min && myInput.value <= max) {
+            letter.classList.remove("alert-warning");
+            letter.classList.remove("alert-danger");
+            letter.classList.add("alert-success");
+            letter.classList.add("valid");
+            letter.classList.remove("invalid");
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.remove("alert-warning");
+            letter.classList.remove("alert-success");
+            letter.classList.add("alert-danger");
+            letter.classList.add("invalid");
+        }
+    }
+
+    }
+
+    
 }
+
+//All the inputs can go here
+//type = 1 = text
+//type = 2 = number
+// validation(index, id, divId, type, min, max, name)
+/* 
+    please follow these rules
+    ===========================
+    index = index before + 1
+    id = input id
+    divId = text under id
+    type = see above
+    min = minimum amount fo characters
+    max = maximum amount of characters
+    name = excel field name 
+    =======================*/
+
+validation(1, 'product-title-1', 'product-title-1-Div', 1, 150, 200, 'Product Title');
+validation(2, 'quantity-1', 'quantity-1-Div', 2, 15, 20, 'Quantity');
+validation(3, 'bullet-point-1', 'bullet-point-1-Div', 1, 0, 500, '1st Bullet Point');
+validation(4, 'bullet-point-2', 'bullet-point-2-Div', 1, 0, 500, '2nd Bullet Point');
